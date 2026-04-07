70cc45359f5ee59bc21e1a15b1066c6b6802bb35
Raychmony00X <shkunov.artyom@mail.ru>
2026-04-07 20:00:50 +0300

    Add files via upload

---
+++ Counter.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+class Counter
+{
+private:
+    int value;
+
+public:
+    Counter() : value(1) {}
+    Counter(int initValue) : value(initValue) {}
+
+    int increase() { return ++value; }
+    int decrease() { return --value; }
+    int getValue() const { return value; }
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string userAnswer;
+    int userValue;
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> userAnswer;
+
+    
+
+    if (userAnswer == "да")
+    {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> userValue;
+
+        Counter counter(userValue);  
+
+        char comand;
+        while (true)
+        {
+            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+            std::cin >> comand;
+
+            if (comand == '+')
+            {
+                counter.increase();
+            }
+            else if (comand == '-')
+            {
+                counter.decrease();
+            }
+            else if (comand == '=')
+            {
+                std::cout << counter.getValue() << std::endl;
+            }
+            else if (comand == 'x')
+            {
+                std::cout << "До свидания!" << std::endl;
+                break;
+            }
+            else
+            {
+                std::cout << "Неверная команда. Попробуйте снова." << std::endl;
+            }
+        }
+    }
+    else if (userAnswer == "нет")
+    {
+        Counter counter;   
+
+        char comand;
+        while (true)
+        {
+            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+            std::cin >> comand;
+
+            if (comand == '+')
+            {
+                counter.increase();
+            }
+            else if (comand == '-')
+            {
+                counter.decrease();
+            }
+            else if (comand == '=')
+            {
+                std::cout << counter.getValue() << std::endl;
+            }
+            else if (comand == 'x')
+            {
+                std::cout << "До свидания!" << std::endl;
+                break;
+            }
+            else
+            {
+                std::cout << "Неверная команда. Попробуйте снова." << std::endl;
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Некорректный ввод. Используется значение по умолчанию (1)." << std::endl;
+
+        Counter counter;  
+
+        char comand;
+        while (true)
+        {
+            std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+            std::cin >> comand;
+
+            if (comand == '+')
+            {
+                counter.increase();
+            }
+            else if (comand == '-')
+            {
+                counter.decrease();
+            }
+            else if (comand == '=')
+            {
+                std::cout << counter.getValue() << std::endl;
+            }
+            else if (comand == 'x')
+            {
+                std::cout << "До свидания!" << std::endl;
+                break;
+            }
+            else
+            {
+                std::cout << "Неверная команда. Попробуйте снова." << std::endl;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ TwoNumbersCalculator.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Calculator
+{
+private:
+	double num1;
+	double num2;
+
+public:
+	
+
+	Calculator() : num1(0.0), num2(0.0)
+	{
+	}
+
+	double add() { return num1 + num2; }
+	double multiply() { return num1 * num2; }
+	double subtract_1_2() { return num2 - num1; }
+	double subtract_2_1() { return num1 - num2; }
+	double divide_1_2() { return num1 / num2; }
+	double divide_2_1() { return num2 / num1; }
+
+	bool set_num1(double newNum1)
+	{
+		if (newNum1 != 0)
+		{
+			num1 = newNum1;
+			return true;
+		}
+		return false;
+	}
+
+	bool set_num2(double newNum2)
+	{
+		if (newNum2 != 0)
+		{
+			num2 = newNum2;
+			return true;
+		}
+		return false;
+	}
+
+};
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	double userNum1, userNum2;
+
+	while (true)
+	{
+		do {
+			std::cout << "Введите num1: ";
+			std::cin >> userNum1;
+			if (userNum1 == 0) {
+				std::cout << "Неверный ввод!\n";
+			}
+		} while (userNum1 == 0);
+
+
+		do {
+			std::cout << "Введите num2: ";
+			std::cin >> userNum2;
+			if (userNum2 == 0) {
+				std::cout << "Неверный ввод!\n";
+			}
+		} while (userNum2 == 0);
+
+
+		Calculator calculator;
+
+		calculator.set_num1(userNum1);
+		calculator.set_num2(userNum2);
+
+		std::cout << "num1 + num2 = " << calculator.add() << std::endl;
+		std::cout << "num1 - num2 = " << calculator.subtract_2_1() << std::endl;
+		std::cout << "num2 - num1 = " << calculator.subtract_1_2() << std::endl;
+		std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
+		std::cout << "num1 / num2 = " << calculator.divide_1_2() << std::endl;
+		std::cout << "num2 / num1 = " << calculator.divide_2_1() << std::endl;
+
+	}
+
+	return 0;
+}
